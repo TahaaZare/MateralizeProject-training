54feb9806160c2526a5830d0019148f82f595e39
Taha Zre <TahaaZre@gmail.com>
2021-09-19 16:54:43 +0430

    Slider

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {
-        edge : 'right',
+        edge: 'right',
     });
+
+    var carouselElems = document.querySelectorAll('.carousel');
+    M.Carousel.init(carouselElems, {
+        fullwidth: true,
+        indicators: true
+    });
+
+    setInterval(function () {
+        M.Carousel.getInstance(carouselElems[0]).next()
+    }, 6000)
 });
